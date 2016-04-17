c63eb48363c8afd538643a30b9cb85af5a28b871
ilaumarius <ilaumarius05@gmail.com>
2016-04-17 12:11:03 +0300

    new update at course

--- Projects/TriangleWebApi/TriangleWebApi/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TriangleWebApi.WebApiApplication" Language="C#" %>
--- Projects/TriangleWebApi/TriangleWebApi/Models/Triangle.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace TriangleWebApi.Models {
-    public class Triangle {
-        public int A { get; set; }
-        public int B { get; set; }
-        public int C { get; set; }
-    }
-}
